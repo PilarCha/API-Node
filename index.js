2743bcc00a261360929828968deabf90cdc1d189
PilarCha <pchavez92@gmail.com>
2021-07-27 18:14:00 -0700

    forgot to put server listen to have server running

--- index.js
+++ index.js
@@ -11,5 +11,11 @@ const db = new sqlite3.Database('./database/employee.db');
 db.run('CREATE TABLE IF NOT EXISTS emp(id TEXT, name TEXT)');
 
 app.get('/', function(req,res) {
-  res.send("<h3> Hi there, You are going to perform CRUD operations......... [CREATE] Please enter 'http://localhost:3000/add/(id number)/(name)' to add new employee to the database.........................[READ] 'http://localhost:3000/view/(id number)' to view an employee.........................[UPDATE] 'http://localhost:3000/update/(id number)/(new name)' to update an employee.....................[DELETE] 'http://localhost:3000/del/(id number)' to delete an employee...............................Before closing this window, kindly enter 'http://localhost:3000/close' to close the database connection <h3>");
+  res.send("<h3> Hi there, You are going to perform CRUD operations.........<br>[CREATE] Please enter 'http://localhost:3000/add/(id number)/(name)' to add new employee to the database.........................<br>[READ] 'http://localhost:3000/view/(id number)' to view an employee.........................<br>[UPDATE] 'http://localhost:3000/update/(id number)/(new name)' to update an employee.....................<br>[DELETE] 'http://localhost:3000/del/(id number)' to delete an employee...............................<BR>Before closing this window, kindly enter 'http://localhost:3000/close' to close the database connection <h3>");
+})
+
+
+
+server.listen(3000,function() {
+  console.log('Server listening on port:3000')
 })
