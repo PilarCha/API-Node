86a6a8a165f9dd5fb18d40540db5fabbafa921db
PilarCha <pchavez92@gmail.com>
2021-07-27 18:08:28 -0700

    adding database directory solved sqlite cant open database file

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const http = require('http');
 
 const app = express();
 const server = http.createServer(app);
-const db = new sqlite3.database('./database/employee.db');
+const db = new sqlite3.Database('./database/employee.db');
 
 db.run('CREATE TABLE IF NOT EXISTS emp(id TEXT, name TEXT)');
+
+app.get('/', function(req,res) {
+  res.send("<h3> Hi there, You are going to perform CRUD operations......... [CREATE] Please enter 'http://localhost:3000/add/(id number)/(name)' to add new employee to the database.........................[READ] 'http://localhost:3000/view/(id number)' to view an employee.........................[UPDATE] 'http://localhost:3000/update/(id number)/(new name)' to update an employee.....................[DELETE] 'http://localhost:3000/del/(id number)' to delete an employee...............................Before closing this window, kindly enter 'http://localhost:3000/close' to close the database connection <h3>");
+})
