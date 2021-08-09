3272b14cdab9882e61c2ea236d73d3914d537bad
PilarCha <pchavez92@gmail.com>
2021-08-08 18:10:43 -0700

    Fields for user id and name

--- client/src/components/CreateUsers.js
+++ client/src/components/CreateUsers.js
@@ -21,38 +21,14 @@ function CreateUsers() {
     <div className={classes.root}>
       <div>
         <TextField
+          label="Users ID"
+          className={classes.textField}
+        />
+        <TextField
           id="standard-full-width"
-          label="Label"
+          label="Full Name"
           style={{ margin: 8 }}
-          placeholder="Placeholder"
-          helperText="Full width!"
           fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField
-          label="None"
-          id="margin-none"
-          defaultValue="Default Value"
-          className={classes.textField}
-          helperText="Some important text"
-        />
-        <TextField
-          label="Dense"
-          id="margin-dense"
-          defaultValue="Default Value"
-          className={classes.textField}
-          helperText="Some important text"
-          margin="dense"
-        />
-        <TextField
-          label="Normal"
-          id="margin-normal"
-          defaultValue="Default Value"
-          className={classes.textField}
-          helperText="Some important text"
           margin="normal"
         />
       </div>
