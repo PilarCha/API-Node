b9442ca66bb184af0cdebb43cec63abfffcaf5bb
PilarCha <pchavez92@gmail.com>
2021-08-10 14:43:54 -0700

    successfully created a custom hook!

--- client/src/components/CreateUsers.js
+++ client/src/components/CreateUsers.js
@@ -2,6 +2,7 @@ import TextField from '@material-ui/core/TextField';
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import { useInput } from '../hooks/input-hook';
 
 function CreateUsers(props) {
   const useStyles = makeStyles((theme) => ({
